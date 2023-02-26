3b8fedcf1df671e825dbf7cac1494528c4befbb4
Lannnnh <15997426361@sina.cn>
2023-02-26 23:17:35 +0800

    update: project 3 kvs-client command init

--- projects/project-3/src/bin/kvs-client.rs
+++ projects/project-3/src/bin/kvs-client.rs
@@ -1,35 +1,77 @@
+use clap::{App, AppSettings, Arg, SubCommand};
 use std::io::{Read, Write};
 use std::net::TcpStream;
 use std::str::from_utf8;
 
 fn main() {
-    match TcpStream::connect("localhost:3333") {
-        Ok(mut stream) => {
-            println!("Successfully connected to server in port 3333");
+    let matches = App::new(env!("CARGO_PKG_NAME"))
+        .version(env!("CARGO_PKG_VERSION"))
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .about(env!("CARGO_PKG_DESCRIPTION"))
+        .setting(AppSettings::DisableHelpSubcommand)
+        .setting(AppSettings::SubcommandRequiredElseHelp)
+        .setting(AppSettings::VersionlessSubcommands)
+        .subcommand(
+            SubCommand::with_name("set")
+                .about("Set the value of a string key to a string")
+                .arg(Arg::with_name("KEY").help("A string key").required(true))
+                .arg(
+                    Arg::with_name("VALUE")
+                        .help("The string value of the key")
+                        .required(true),
+                ),
+        )
+        .subcommand(
+            SubCommand::with_name("get")
+                .about("Get the string value of a given string key")
+                .arg(Arg::with_name("KEY").help("A string key").required(true)),
+        )
+        .subcommand(
+            SubCommand::with_name("rm")
+                .about("Remove a given key")
+                .arg(Arg::with_name("KEY").help("A string key").required(true)),
+        )
+        .get_matches();
 
-            let msg = b"Hello!";
+    // match TcpStream::connect("localhost:3333") {
+    //     Ok(mut stream) => {
+    //         println!("Successfully connected to server in port 3333");
 
-            stream.write(msg).unwrap();
-            println!("Sent Hello, awaiting reply...");
+    //         let msg = b"Hello!";
 
-            let mut data = [0 as u8; 6]; // using 6 byte buffer
-            match stream.read_exact(&mut data) {
-                Ok(_) => {
-                    if &data == msg {
-                        println!("Reply is ok!");
-                    } else {
-                        let text = from_utf8(&data).unwrap();
-                        println!("Unexpected reply: {}", text);
-                    }
-                }
-                Err(e) => {
-                    println!("Failed to receive data: {}", e);
-                }
-            }
+    //         stream.write(msg).unwrap();
+    //         println!("Sent Hello, awaiting reply...");
+
+    //         let mut data = [0 as u8; 6]; // using 6 byte buffer
+    //         match stream.read_exact(&mut data) {
+    //             Ok(_) => {
+    //                 if &data == msg {
+    //                     println!("Reply is ok!");
+    //                 } else {
+    //                     let text = from_utf8(&data).unwrap();
+    //                     println!("Unexpected reply: {}", text);
+    //                 }
+    //             }
+    //             Err(e) => {
+    //                 println!("Failed to receive data: {}", e);
+    //             }
+    //         }
+    //     }
+    //     Err(e) => {
+    //         println!("Failed to connect: {}", e);
+    //     }
+    // }
+
+    match matches.subcommand() {
+        ("set", Some(_matches)) => {
+            eprintln!("unimplement!");
         }
-        Err(e) => {
-            println!("Failed to connect: {}", e);
+        ("get", Some(_matches)) => {
+            eprintln!("unimplement!");
         }
+        ("rm", Some(_matches)) => {
+            eprintln!("unimplement!");
+        }
+        _ => unreachable!(),
     }
-    println!("Terminated.");
 }
